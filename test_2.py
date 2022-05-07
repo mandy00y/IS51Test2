363544b3df71374ce550b818106aff314a83139d
Your Name <you@example.com>
2022-05-06 21:13:26 -0700

    adding laymen's

--- test_2.py
+++ test_2.py
@@ -1,5 +1,17 @@
 
+"""
+
+There is a file called Final.txt. You want to open it and count how 
+many grades there are. The program will tell you how many grades there
+are in the file, and the average grade. It will also tell you the 
+percentage of grades that are above the average grade.
+
+Main function will kickstart the program, open the file, and 
+output 24 total grades and output average 83.25
+
+Within the main function, the calculate_percent_above_average function
+will output average percentage above average 54.17%.
 
 
-get_history_length
+"""
 
